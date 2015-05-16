9604633d40c7aeace0f66b2f6487fcc47f616eaa
laxmikant4644 <laxmikant.4644@gmail.com>
2015-05-16 19:11:53 +0530

    process search and view added

--- SmartAdjust1.0/Form1.Designer.cs
+++ SmartAdjust1.0/Form1.Designer.cs
@@ -28,49 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // treeView1
             // 
-            this.button1.Location = new System.Drawing.Point(206, 86);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Focus";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(142, 34);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(261, 20);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.treeView1.Location = new System.Drawing.Point(59, 36);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(496, 392);
+            this.treeView1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(43, 37);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(231, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(82, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Process Name :";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.Size = new System.Drawing.Size(135, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Process Tree";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(263, 445);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "End Process";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(450, 130);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(620, 490);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.treeView1);
             this.Name = "Form1";
             this.Text = "Smart Adjust";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -78,9 +77,10 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TreeView treeView1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+
     }
 }
 
--- SmartAdjust1.0/Form1.cs
+++ SmartAdjust1.0/Form1.cs
@@ -12,6 +12,9 @@ namespace SmartAdjust1._0
 {
     public partial class Form1 : Form
     {
+
+        Process[] processlist = Process.GetProcesses();
+
         public Form1()
         {
             InitializeComponent();
@@ -19,12 +22,12 @@ namespace SmartAdjust1._0
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if(Process.GetProcessesByName(textBox1.Text).Length >=1){
-                foreach(Process p in Process.GetProcessesByName(textBox1.Text)){
-                    MessageBox.Show(p.ProcessName);
-                    p.CloseMainWindow();
-                }
-            }
+            //if(Process.GetProcessesByName(textBox1.Text).Length >=1){
+            //    foreach(Process p in Process.GetProcessesByName(textBox1.Text)){
+            //        MessageBox.Show(p.ProcessName);
+            //        p.CloseMainWindow();
+            //    }
+            //}
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -36,5 +39,27 @@ namespace SmartAdjust1._0
         {
 
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach(Process theprocess in processlist){
+                //MessageBox.Show(theprocess.ProcessName + " " + treeView1.Nodes.IndexOfKey(theprocess.ProcessName));
+      
+                if (treeView1.Nodes.IndexOfKey(theprocess.ProcessName) > -1)
+                {
+                    //MessageBox.Show("Duplicate");
+                    treeView1.Nodes[treeView1.Nodes.IndexOfKey(theprocess.ProcessName)].Nodes.Add("" + theprocess.Id, "" + theprocess.Id);
+                    //treeView1.Nodes.Add(node);
+                }
+                else
+                {
+                    treeView1.Nodes.Add(theprocess.ProcessName, theprocess.ProcessName);
+                    treeView1.Nodes[treeView1.Nodes.Count - 1].Nodes.Add("" + theprocess.Id, "" + theprocess.Id);   
+                }
+                
+            }
+            MessageBox.Show("" + treeView1.Nodes.Count);
+        }
+        
     }
 }

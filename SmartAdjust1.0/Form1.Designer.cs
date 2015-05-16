6a1d3d6fc5f37f28634ddf64089f72c094e7d957
laxmikant4644 <laxmikant.4644@gmail.com>
2015-05-16 20:39:00 +0530

    Refresh List and End Process functionality added

--- SmartAdjust1.0/Form1.Designer.cs
+++ SmartAdjust1.0/Form1.Designer.cs
@@ -31,14 +31,17 @@
             this.treeView1 = new System.Windows.Forms.TreeView();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // treeView1
             // 
+            this.treeView1.CheckBoxes = true;
             this.treeView1.Location = new System.Drawing.Point(59, 36);
             this.treeView1.Name = "treeView1";
             this.treeView1.Size = new System.Drawing.Size(496, 392);
             this.treeView1.TabIndex = 0;
+            this.treeView1.AfterCheck += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterCheck);
             // 
             // label1
             // 
@@ -52,18 +55,30 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(263, 445);
+            this.button1.Location = new System.Drawing.Point(352, 445);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
             this.button1.Text = "End Process";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(195, 445);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Refresh List";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(620, 490);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.treeView1);
@@ -80,6 +95,7 @@
         private System.Windows.Forms.TreeView treeView1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
 
     }
 }
--- SmartAdjust1.0/Form1.cs
+++ SmartAdjust1.0/Form1.cs
@@ -8,12 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
+
 namespace SmartAdjust1._0
 {
     public partial class Form1 : Form
     {
 
-        Process[] processlist = Process.GetProcesses();
+        Process[] processlist;
 
         public Form1()
         {
@@ -30,36 +31,112 @@ namespace SmartAdjust1._0
             //}
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
         private void Form1_Load(object sender, EventArgs e)
         {
-            foreach(Process theprocess in processlist){
+            loadAllProcesses();
+        }
+
+        private void loadAllProcesses()
+        {
+            treeView1.Nodes.Clear();
+
+            processlist = Process.GetProcesses();
+
+            foreach (Process theprocess in processlist)
+            {
                 //MessageBox.Show(theprocess.ProcessName + " " + treeView1.Nodes.IndexOfKey(theprocess.ProcessName));
-      
+
                 if (treeView1.Nodes.IndexOfKey(theprocess.ProcessName) > -1)
                 {
                     //MessageBox.Show("Duplicate");
-                    treeView1.Nodes[treeView1.Nodes.IndexOfKey(theprocess.ProcessName)].Nodes.Add("" + theprocess.Id, "" + theprocess.Id);
+                    treeView1.Nodes[treeView1.Nodes.IndexOfKey(theprocess.ProcessName)].Nodes.Add("" + theprocess.Id, "" + theprocess.Id + "    "+theprocess.MainWindowTitle);
                     //treeView1.Nodes.Add(node);
                 }
                 else
                 {
                     treeView1.Nodes.Add(theprocess.ProcessName, theprocess.ProcessName);
-                    treeView1.Nodes[treeView1.Nodes.Count - 1].Nodes.Add("" + theprocess.Id, "" + theprocess.Id);   
+                    treeView1.Nodes[treeView1.Nodes.Count - 1].Nodes.Add("" + theprocess.Id, "" + theprocess.Id + "    " + theprocess.MainWindowTitle);
                 }
-                
+
             }
-            MessageBox.Show("" + treeView1.Nodes.Count);
+
+            //treeView1.Sort();
         }
-        
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Boolean found = false;
+            for (int j=treeView1.Nodes.Count-1; j>-1; j--)
+            {
+                TreeNode n = treeView1.Nodes[j];
+                for (int i=n.Nodes.Count-1;i>-1;i--)
+                {
+                    //MessageBox.Show(cn.Text);
+                    if (n.Nodes[i].Checked)
+                    {
+                        //MessageBox.Show(cn.Text);
+                        found=true;
+                        try
+                        {
+                            Process p = Process.GetProcessById(Convert.ToInt32(n.Nodes[i].Text.Split(' ')[0]));
+                            p.Kill();
+                        }
+                        catch(Exception exp)
+                        {
+                            MessageBox.Show(exp.Message,"Error");
+                        }
+
+                        n.Nodes.RemoveAt(i);
+                        //cn = n.Nodes[0];
+                    }
+                }
+
+                if (n.Nodes.Count==0)
+                {
+                    treeView1.Nodes.RemoveAt(j);
+                }
+
+            }
+
+            if (!found)
+            {
+                MessageBox.Show("Please Select a process first.");
+            }
+
+
+            loadAllProcesses();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            loadAllProcesses();
+        }
+
+        private void treeView1_AfterCheck(object sender, TreeViewEventArgs e)
+        {
+            if (e.Action != TreeViewAction.Unknown)
+            {
+                if (e.Node.Nodes.Count > 0)
+                {
+                    /* Calls the CheckAllChildNodes method, passing in the current 
+                    Checked value of the TreeNode whose checked state changed. */
+                    this.CheckAllChildNodes(e.Node, e.Node.Checked);
+                }
+            }
+            //MessageBox.Show(treeView1.Nodes.IndexOf(TreeNode(sender.text).Text);
+        }
+
+        private void CheckAllChildNodes(TreeNode treeNode, bool nodeChecked)
+        {
+            foreach (TreeNode node in treeNode.Nodes)
+            {
+                node.Checked = nodeChecked;
+                if (node.Nodes.Count > 0)
+                {
+                    // If the current node has child nodes, call the CheckAllChildsNodes method recursively. 
+                    this.CheckAllChildNodes(node, nodeChecked);
+                }
+            }
+        }
     }
 }

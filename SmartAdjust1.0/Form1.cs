2759cf39044a2d317813fd26ac1b6002072d5eb4
laxmikant4644 <laxmikant.4644@gmail.com>
2015-05-16 17:53:36 +0530

    main file for app added

---
+++ SmartAdjust1.0/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+namespace SmartAdjust1._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(Process.GetProcessesByName(textBox1.Text).Length >=1){
+                foreach(Process p in Process.GetProcessesByName(textBox1.Text)){
+                    MessageBox.Show(p.ProcessName);
+                    p.CloseMainWindow();
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
